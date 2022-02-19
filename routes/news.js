2844be4b4cde73446e6cfa4c68ca891263992fe9
OsamaHayyan <sonikx2011@gmail.com>
2022-02-19 15:52:52 +0200

    Explained each Route in News

--- routes/news.js
+++ routes/news.js
@@ -12,10 +12,52 @@ const { error_validation } = require("../middleware/error_validation");
 const router = express.Router();
 const uploads = require("../middleware/uploads").single("poster");
 
+/*
+@POST: /news/create-news/
+@Super_User
+@Request : {
+  "topic": String,
+  "title": String,
+  "poster": File
+}
+@Response: "success"
+*/
 router.post("/create-news", is_superuser, uploads, error_validation, postNews);
 
+/*
+@GET: /news/
+@user
+@Query: {
+  page: string or number ==> optional,
+}
+@Response:
+{
+ news: [{
+  "_id": string,
+  "title": string,
+  "topic": string,
+  "poster": string,
+  "createdAt": string,
+  "updatedAt": string,
+}]
+ newsPages: string
+}
+*/
 router.get("/", getAllNews);
 
+/*
+@GET: /news/get-news/:newsId
+@User
+@Params: newsId
+@Response: {
+  "_id": string,
+  "title": string,
+  "topic": string,
+  "poster": string,
+  "createdAt": string,
+  "updatedAt": string,
+}
+*/
 router.get(
   "/get-news/:newsId",
   param("newsId", "Please Choose correct news").isMongoId(),
@@ -23,6 +65,24 @@ router.get(
   getNews
 );
 
+/*
+@PUT: /news/put-news/:newsId
+@Super_User
+@Params: newsId
+@Request : {
+  "topic": String,
+  "title": String,
+  "poster": File
+}
+@Response: {
+  "_id": string,
+  "title": string,
+  "topic": string,
+  "poster": string,
+  "createdAt": string,
+  "updatedAt": string,
+}
+*/
 router.put(
   "/put-news/:newsId",
   is_superuser,
@@ -31,6 +91,17 @@ router.put(
   putNews
 );
 
+/*
+@DELETE: /news/delete-news/:newsId
+@Super_User
+@Params: newsId
+@Request : {
+  "topic": String,
+  "title": String,
+  "poster": File
+}
+@Response: "Deleted"
+*/
 router.delete(
   "/delete-news/:newsId",
   is_superuser,
