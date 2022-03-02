de0596844edf0e4276baba3d6baedda9b51acf05
OsamaHayyan <sonikx2011@gmail.com>
2022-03-02 16:36:18 +0200

    Clearified Manga routes

--- routes/manga.js
+++ routes/manga.js
@@ -17,7 +17,7 @@ const {
   error_validation,
 } = require("../middleware/error_validation");
 const { is_admin, is_superuser } = require("../middleware/authorisation");
-const { param, query } = require("express-validator");
+const { param } = require("express-validator");
 
 const uploads = require("../middleware/uploads").fields([
   { name: "image", maxCount: 1 },
@@ -26,39 +26,25 @@ const uploads = require("../middleware/uploads").fields([
 
 /*
 @GET: /mangas/
-@Admin
+@User
 @Query: {
   CatId: _id ==> optional,
   orderBy: (alphabet, view, new, rate),
   page: string or number ==> optional,
 }
-@Response: {
-  title: String,
-  category: Array of strings
-  story: String
-  image: String
-  banner: String
-  status: String
-  date: String
-  auther: String
-  views: String
-  rate: String,
-  createdAt: String
+@Response:  {
+   "mangaData": [
+        {
+            "_id": String,
+            "title": String,
+            "image": String,
+            "views": Int
+        }
+      ],
+    "mangaPages": Int
 }
 */
-router.get(
-  "/",
-  // query("catId", "Please choose valid category")
-  //   .if((value) => {
-  //     if (value) {
-  //       return true;
-  //     }
-  //     return false;
-  //   })
-  //   .isMongoId(),
-  // error_validation,
-  getAllManga
-);
+router.get("/", getAllManga);
 
 /*
 @POST: /mangas/add
@@ -70,8 +56,8 @@ router.get(
   date: String Date ==> required,
   category: string or array of strings ==> required,
   auther: _id as string ==> required,
-  imageUrl: string path ==> required,
-  bannerUrl: string path ==> optional
+  image: string path ==> required,
+  banner: string path ==> optional
 },
 @Response: {
   message: "success"
@@ -79,6 +65,36 @@ router.get(
 */
 router.post("/add", is_admin, uploads, manga_validation, createManga);
 
+/*
+@GET: /mangas/manga/:mangaId
+@User
+@Params: mangaId
+@Response: {
+  title: String,
+  category: [
+        {
+            "_id": String,
+            "category": String
+        }
+    ]
+  story: String
+  image: String
+  banner: String
+  status: String
+  date: String
+  auther: String
+  views: Int
+  rate: Int,
+  chapters: [
+        {
+            "chapterNum": String,
+            "name": String,
+            "views": Int,
+            "date": String,
+            "_id": String
+        }
+}     ]
+*/
 router.get(
   "/manga/:mangaId",
   param("mangaId", "Manga Not Found").isMongoId(),
@@ -86,10 +102,47 @@ router.get(
   getManga
 );
 
+/*
+@GET: /mangas/most-viewed
+@User
+@Response: [
+  {
+      "_id": String,
+      "title": String,
+      "image": String,
+      "views": Int
+  }
+]
+*/
 router.get("/most-viewed", mostViewed);
 
+/*
+@PUT: /mangas/manga/:mangaId"
+@Admin
+@Params: mangaId
+@Request: {
+  Title: { min: 5, max: 100 } ==> required,
+  Story: { min: 10 } ==> required,
+  status: {"on going" || "finished" || "stopped" ||  "مستمرة" || "منتهية " || "متوقفة"} ==> required,
+  date: String Date ==> required,
+  category: string or array of strings ==> required,
+  auther: _id as string ==> required,
+  image: string path ==> optional,
+  banner: string path ==> optional
+},
+@Response: { message: "success" }
+*/
 router.put("/manga/:mangaId", is_admin, uploads, manga_validation, putManga);
 
+/*
+@DELETE: /mangas/delete/:mangaId
+@Super_User
+@Params: mangaId
+@Request: {
+  mangaId: String ==> Required
+},
+@Response: { message: "deleted succefully" }
+*/
 router.delete(
   "/delete/:mangaId",
   is_superuser,
@@ -98,8 +151,33 @@ router.delete(
   deleteManga
 );
 
+/*
+@POST: /mangas/search-manga
+@User
+@Request: {
+  query : String ==> Required
+},
+@Response: [
+    {
+        "_id": String,
+        "title": String,
+        "image": String
+    }
+  ]
+*/
 router.post("/search-manga", searchManga);
 
+/*
+@POST: /mangas/rate/:mangaId
+@User
+@Params: mangaId
+@Request: {
+  query : String ==> Required
+},
+@Response: {
+    "rate": String number
+  }
+*/
 router.post(
   "/rate/:mangaId",
   param("mangaId", "Manga Not Found").isMongoId(),
@@ -107,6 +185,14 @@ router.post(
   postRating
 );
 
+/*
+@GET: /mangas/rate/:mangaId
+@User
+@Params: mangaId
+@Response: {
+    "rate": Int
+  }
+*/
 router.get(
   "/rate/:mangaId",
   param("mangaId", "Manga Not Found").isMongoId(),
