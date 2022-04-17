8dbf93e7c3a7d0b0e9f51960dec65e7a810cdc7f
OsamaHayyan <sonikx2011@gmail.com>
2022-04-17 04:11:36 +0200

    Changed CORP option to same-site

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const corsOptions = {
 
 const app = express();
 
-app.use(helmet());
+app.use(helmet({ crossOriginResourcePolicy: { policy: "same-site" } }));
 app.use(compression());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
