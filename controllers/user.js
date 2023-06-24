5339b3062106d9358c6f7f3dcb8c06c72f614c01
osamahayyan <osamahayyan1567@gmail.com>
2023-06-24 08:21:04 +0300

    changed domain

--- controllers/user.js
+++ controllers/user.js
@@ -64,7 +64,7 @@ export const login = async (req, res, next) => {
 
     return res
       .cookie("access_token", token, {
-        domain: "https://mymanga.vercel.app",
+        domain: "mymanga.vercel.app",
         path: "/user/login",
         expires: new Date(Date.now() + 3 * (60 * 60 * 1000)),
         httpOnly: true,
