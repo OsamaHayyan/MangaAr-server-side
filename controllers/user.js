a0407f20750d6b696d2a4d7b7048615fc91879af
osamahayyan <osamahayyan1567@gmail.com>
2023-06-24 09:39:06 +0300

    changed cookie expire time

--- controllers/user.js
+++ controllers/user.js
@@ -64,7 +64,7 @@ export const login = async (req, res, next) => {
 
     return res
       .cookie("access_token", token, {
-        expires: new Date(Date.now() + 3 * (60 * 60 * 1000)),
+        expires: new Date(Date.now() + 6 * (60 * 60 * 1000)),
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "strict",
