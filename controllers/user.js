cf7562368a62a112f16b5fd425fc36a73a32fd54
osamahayyan <osamahayyan1567@gmail.com>
2023-06-24 09:38:23 +0300

    changed samesite to strict

--- controllers/user.js
+++ controllers/user.js
@@ -64,11 +64,10 @@ export const login = async (req, res, next) => {
 
     return res
       .cookie("access_token", token, {
-        path: "/user/login",
         expires: new Date(Date.now() + 3 * (60 * 60 * 1000)),
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "none",
+        sameSite: "strict",
       })
       .status(201)
       .json({
